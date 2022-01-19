4e2a7db52a8d5dce3ece81faa647e1f07bc81d1d
Alfredo Parra <alfredo-920@hotmail.com>
2022-01-19 10:05:30 -0500

    Se creó la matríz en JS y la verificación de las bombas en la coordenada dada

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,31 @@ Añade al algoritmo la validación de que sean números enteros
 */
 
 //* Declaración de variables
-let ejeX;
-let ejeY;
+let fila;
+let columna;
+let hayBomba = false;
+let tablero;
 
-//* Ingreso de datos
-ejeY = parseInt(prompt("Ingrese el número de la fila:"));
-ejeX = parseInt(prompt("Ingrese el número de la columna:"));
+/* Inicialización del tablero, los ceros representan las posiciones
+seguras, los unos representan las bombas */
+tablero = [[0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [1, 0, 0, 1]];
 
-//* Validación
-if (ejeY <= 0 || ejeY > 4)
+do {
+  //* Ingreso de datos
+  fila = parseInt(prompt("Ingrese el número de la fila:"));
+  columna = parseInt(prompt("Ingrese el número de la columna:"));
+
+  //* Validación de las coordenadas
+  if (fila <= 0 || fila > 4)
     alert("Número de fila incorrecto.");
-else if (ejeX <= 0 || ejeX > 4)
-    alert("Número de columna incorrecto.")
+  else if (columna <= 0 || columna > 4)
+    alert("Número de columna incorrecto.");
+
+  // Validación de la posición respecto a las bombas
+  if (tablero[fila - 1][columna - 1] == 1) {
+    alert("Kaboooooooooooommmm!!!!");
+    hayBomba = true;
+  } else {
+    alert("Bien hecho, una vez más");
+  }
+}while (!hayBomba);
