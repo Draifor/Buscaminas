0817735f0335ceaf5886890a3e942d95b55ff8c4
Alfredo Parra <alfredo-920@hotmail.com>
2022-02-02 23:10:54 -0500

    Diseño y juego 'terminado' - Falta poner la minas más aleatoreamente y destapar las casillas vacías

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 // ** Declaración de las constantes **
-const CANT_FILAS = 4;
-const CANT_COLUMNAS = 4;
-const CANT_MINAS = 6;
+const CANT_FILAS = 10;
+const CANT_COLUMNAS = 10;
+const CANT_CASILLAS = CANT_FILAS * CANT_COLUMNAS;
+const CANT_MINAS = 30;
 
 // ** Declaración de variables **
-let detonarMina = false;
+let finJuego = false;
+let contadorDestapadas = 0;
+const parrafo = document.createElement("P");
+const botonNuevoJuego = document.querySelector(".left button");
+const sectionRight = document.querySelector("section.right");
+const puntaje = document.getElementById("puntaje");
+const cuadricula = document.querySelector("div.cuadricula");
 
 // ** Definición de la clase **
 class Tablero {
@@ -16,120 +23,188 @@ class Tablero {
   }
 
   // ** Definición de las funciones de la clase **
-
   // Función para crear la matriz con las respectivas minas en posiciones aleatorias
   crearTablero = () => {
     // Variables auxiliares
     let codigo;
     let contador = 0;
 
+    // Crear la matriz y usar la función añadirCasilla() para crear los botones del tablero
     while (contador < CANT_MINAS) {
       for (let i = 0; i < CANT_FILAS; i++) {
         this.matriz.push([]);
-        while (this.matriz[i].length < CANT_COLUMNAS) {
+        for (let j =  0; this.matriz[i].length < CANT_COLUMNAS;) {
           codigo = Math.round(Math.random());
           if (codigo == 1 && contador < CANT_MINAS) {
             this.matriz[i].push(codigo);
             contador++;
           } else if (codigo == 0) this.matriz[i].push(codigo);
+          else continue;
+
+          const casillaActual = {};
+          casillaActual.fila = i;
+          casillaActual.columna = j;
+
+          this.añadirCasilla(casillaActual);
+          j++;
         }
       }
+    };
+  }
+
+  // Crear y añadir casillas (botones) al tablero
+  añadirCasilla = (casillaActual) => {
+    casillaActual.boton = document.createElement("BUTTON");
+    casillaActual.boton.id = casillaActual.fila + "" + casillaActual.columna;
+    casillaActual.boton.onclick = () => {
+      if (!finJuego && !casillaActual.boton.classList.contains("bandera")){
+        this.evaluar(casillaActual);
+      }
     }
-  };
+    casillaActual.boton.oncontextmenu = function(e) {
+      e.preventDefault();
+      ponerBandera(casillaActual);
+    }
+    cuadricula.appendChild(casillaActual.boton);
+  }
 
   // Función para evaluar las coordenadas recibidas
-  evaluar = (fila, columna) => {
-    // Evaluar el rango de las coordenadas
-    if (fila < 0 || fila + 1 > this.matriz.length)
-      return alert("Número de fila incorrecto.");
-    else if (columna < 0 || columna + 1 > this.matriz[fila].length)
-      return alert("Número de columna incorrecto.");
-
+  evaluar = (casillaActual) => {
     // Validación de la posición respecto a las minas
-    if (this.matriz[fila][columna] == 1) {
-      alert("Kaboooooooooooommmm!!!!");
-      detonarMina = true;
-    } else alert(`Hay ${minasAdyacentes(fila, columna)} minas alrededor`);
+    if (this.matriz[casillaActual.fila][casillaActual.columna] == 1) {
+      finJuego = true;
+      casillaActual.boton.classList.add("bombaDet")
+      casillaActual.boton.innerHTML = '<img class="img-bombaDet" src="img/bomba_det2.png" alt="explosion_bomba">';
+      mostrarBombas(casillaActual);
+      // puntaje.innerText = " " + Math.round((CANT_CASILLAS) * (contadorDestapadas / CANT_CASILLAS));
+      parrafo.innerText = "¡Perdiste! Inténtalo de nuevo"
+      sectionRight.appendChild(parrafo);
+    } else {
+      if (!casillaActual.boton.classList.contains("destapada")){
+        const minas = minasAdyacentes(casillaActual);
+        if (minas == 0) {
+          destaparCasillasVacias(casillaActual);
+          casillaActual.boton.classList.add("destapada");
+        } else {
+          casillaActual.boton.appendChild(document.createTextNode(minas));
+          casillaActual.boton.style.backgroundColor = "rgba(101,178,198,1)";
+          casillaActual.boton.classList.add("destapada");
+          }
+          contadorDestapadas++;
+          puntaje.innerText = " " + Math.round((CANT_CASILLAS) * (contadorDestapadas / CANT_CASILLAS)) * 10;
+          if (contadorDestapadas == CANT_CASILLAS - CANT_MINAS) {
+            puntaje.innerText = " " + CANT_CASILLAS * 10;
+            parrafo.innerText = "¡Ganaste! Enhorabuena"
+            sectionRight.appendChild(parrafo);
+          }
+        }
+    }
   }
 };
 
 // ** Definición de las funciones **
 
-// Función para pedir las coordenadas
-const jugar = (tablero) => {
-  let fila = parseInt(prompt("Ingrese el número de la fila:")) - 1;
-  let columna = parseInt(prompt("Ingrese el número de la columna:")) - 1;
-
-  tablero.evaluar(fila, columna);
-};
-
 // Función para comprobar la cantidad de minas adyacentes
-const minasAdyacentes = (fila, columna) => {
+const minasAdyacentes = (casillaActual) => {
   let contador = 0;
 
-  if (fila == 0) {
-    if (columna == 0) {
-      contador += tablero.matriz[fila][columna + 1];
-      contador += tablero.matriz[fila + 1][columna];
-      contador += tablero.matriz[fila + 1][columna + 1];
-    } else if (columna > 0 && columna < CANT_COLUMNAS - 1) {
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila][columna + 1];
-      contador += tablero.matriz[fila + 1][columna - 1];
-      contador += tablero.matriz[fila + 1][columna];
-      contador += tablero.matriz[fila + 1][columna + 1];
+  if (casillaActual.fila == 0) {
+    if (casillaActual.columna == 0) {
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna + 1];
+    } else if (casillaActual.columna > 0 && casillaActual.columna < CANT_COLUMNAS - 1) {
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna + 1];
     } else {
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila + 1][columna - 1];
-      contador += tablero.matriz[fila + 1][columna];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
     }
-  } else if (fila > 0 && fila < CANT_FILAS - 1) {
-    if (columna == 0) {
-      contador += tablero.matriz[fila - 1][columna];
-      contador += tablero.matriz[fila - 1][columna + 1];
-      contador += tablero.matriz[fila][columna + 1];
-      contador += tablero.matriz[fila + 1][columna];
-      contador += tablero.matriz[fila + 1][columna + 1];
-    } else if (columna > 0 && columna < CANT_COLUMNAS - 1) {
-      contador += tablero.matriz[fila - 1][columna - 1];
-      contador += tablero.matriz[fila - 1][columna];
-      contador += tablero.matriz[fila - 1][columna + 1];
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila][columna + 1];
-      contador += tablero.matriz[fila + 1][columna - 1];
-      contador += tablero.matriz[fila + 1][columna];
-      contador += tablero.matriz[fila + 1][columna + 1];
+  } else if (casillaActual.fila > 0 && casillaActual.fila < CANT_FILAS - 1) {
+    if (casillaActual.columna == 0) {
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna + 1];
+    } else if (casillaActual.columna > 0 && casillaActual.columna < CANT_COLUMNAS - 1) {
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna + 1];
     } else {
-      contador += tablero.matriz[fila - 1][columna - 1];
-      contador += tablero.matriz[fila - 1][columna];
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila + 1][columna - 1];
-      contador += tablero.matriz[fila + 1][columna];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila + 1][casillaActual.columna];
     }
   } else {
-    if (columna == 0) {
-      contador += tablero.matriz[fila - 1][columna];
-      contador += tablero.matriz[fila - 1][columna + 1];
-      contador += tablero.matriz[fila][columna + 1];
-    } else if (columna > 0 && columna < CANT_COLUMNAS - 1) {
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila][columna + 1];
-      contador += tablero.matriz[fila - 1][columna - 1];
-      contador += tablero.matriz[fila - 1][columna];
-      contador += tablero.matriz[fila - 1][columna + 1];
+    if (casillaActual.columna == 0) {
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+    } else if (casillaActual.columna > 0 && casillaActual.columna < CANT_COLUMNAS - 1) {
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna + 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna + 1];
     } else {
-      contador += tablero.matriz[fila][columna - 1];
-      contador += tablero.matriz[fila - 1][columna - 1];
-      contador += tablero.matriz[fila - 1][columna];
+      contador += tablero.matriz[casillaActual.fila][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna - 1];
+      contador += tablero.matriz[casillaActual.fila - 1][casillaActual.columna];
     }
   }
   return contador;
 };
 
-// ** Inicio del juego **
-// const tablero = new Tablero(CANT_FILAS, CANT_COLUMNAS, CANT_MINAS);
-// console.log(tablero.matriz);
+// Función para destapar las casillas adyacentes que no tengan minas alrededor;
+const destaparCasillasVacias = (casillaActual) => {
+  casillaActual.boton.style.backgroundColor = "rgba(101,178,198,1)";
+  let idCasilla;
+  if (casillaActual.fila == 0) {
+    if (casillaActual.columna == 0) {
+      idCasilla = casillaActual.fila + 1;
+    }
+  }
+}
 
-// do {
-//   jugar(tablero);
-// } while (!detonarMina);
+// Función para poner banderas
+const ponerBandera = (casillaActual) => {
+  if (!finJuego && !casillaActual.boton.classList.contains("destapada")){
+    if (casillaActual.boton.classList.toggle("bandera")) casillaActual.boton.innerHTML = '<img class="img-bandera" src="img/bandera.png">';
+    else casillaActual.boton.innerText = "";
+  }
+}
+
+// Función para mostrar todas las bombas al terminar la partida
+const mostrarBombas = (casillaActual) => {
+  for (let i = 0; i < CANT_FILAS; i++) {
+    for (let j = 0; j < CANT_COLUMNAS; j++){
+      if (tablero.matriz[i][j] == 1) {
+        const idCasilla = i + "" + j;
+        const casillaBomba = document.getElementById(idCasilla);
+        if (casillaActual.boton.id != idCasilla) {
+          casillaBomba.classList.add("bomba");
+          casillaBomba.innerHTML = '<img class="img-bomba" src="img/bomba.png">';
+        }
+      }
+    }
+  }
+}
+// Refrescar la página para iniciar un nuevo juego
+botonNuevoJuego.onclick = () => {
+  location.reload();
+}
+// ** Inicio del juego **
+const tablero = new Tablero();
+console.log(tablero.matriz);
